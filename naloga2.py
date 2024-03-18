0aa5fc0bc950c1ccb1c4d51213d649630c817114
Grega Zorko <Zorko.Grega@gmail.com>
2024-03-18 14:12:27 +0100

    Implementiral funkcijo konvolucija.

--- naloga2.py
+++ naloga2.py
@@ -2,9 +2,26 @@ import cv2 as cv
 import numpy as np
 
 def konvolucija(slika, jedro):
-    '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
-    Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
-    pass
+    višina, širina = slika.shape
+    višina_jedra, širina_jedra = jedro.shape
+    
+    # Ustvarjanje izhodne slike polne ničel
+    izhod = np.zeros((višina, širina), dtype=np.float32)
+    
+    # Računanje robov za padding
+    pad_height = višina_jedra // 2
+    pad_width = širina_jedra // 2
+    
+    # Dodajanje paddinga k vhodni sliki
+    padded_image = np.pad(slika, ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=0)
+    
+    # Izvajanje konvolucije
+    for i in range(višina):
+        for j in range(širina):
+            # Izračun konvolucije za vsako slikovno piko
+            izhod[i, j] = np.sum(jedro * padded_image[i:i+višina_jedra, j:j+širina_jedra])
+    
+    return izhod
 
 def filtriraj_z_gaussovim_jedrom(slika,sigma):
     '''Filtrira sliko z Gaussovim jedrom..'''
