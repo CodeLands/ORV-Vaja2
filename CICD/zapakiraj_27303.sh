d475d3d987ae5e61124a8332e49a65341f03483e
Grega Zorko <Zorko.Grega@gmail.com>
2024-04-14 13:54:14 +0200

    fixed envs in deploy.yml script

--- CICD/zapakiraj_27303.sh
+++ CICD/zapakiraj_27303.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+# Exit immediately if a command exits with a non-zero status and for any failures in pipelines.
+set -eo pipefail
 
 # Load Docker environment variables, if any
 if [ -f .env ]; then
-  export $(cat .env | sed 's/#.*//g' | xargs)
+  export $(grep -v '^#' .env | xargs)
 fi
 
-# Build the Docker image
+# Check if DOCKER_USERNAME is set
+if [ -z "$DOCKER_USERNAME" ]; then
+    echo "DOCKER_USERNAME is not set. Exiting..."
+    exit 1
+fi
+
+# Build the Docker image with the commit SHA as a tag
 docker build -t myapp:${GITHUB_SHA} .
 
 # Tag the image for the repository on DockerHub
@@ -16,3 +22,5 @@ docker tag myapp:${GITHUB_SHA} $DOCKER_USERNAME/myapp:latest
 
 # Push the image to DockerHub
 docker push $DOCKER_USERNAME/myapp:latest
+
+echo "Docker image pushed successfully!"
