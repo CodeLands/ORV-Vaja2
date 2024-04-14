9ec0efd21b52c9a2a650ab32dbb31b30459f826a
Grega Zorko <zorko.grega@gmail.com>
2024-04-14 22:02:15 +0200

    dockerfile fixes

--- zazeni_27303.sh
+++ zazeni_27303.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pull the latest version of the image from DockerHub
-docker pull $DOCKER_USERNAME/myapp:latest
+docker pull pckill3r/myapp:latest
 
-# Run the Docker container
-docker run -d -p 80:80 $DOCKER_USERNAME/myapp:latest
+# Run the Docker container with host display
+docker run -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix pckill3r/myapp:latest
