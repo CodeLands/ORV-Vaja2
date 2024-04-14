3048b4e7ba0af6b7667b2a0ecf992fba429c52c2
Grega Zorko <Zorko.Grega@gmail.com>
2024-04-14 14:04:06 +0200

    fixed zapakiraj script to take in args

--- zapakiraj_27303.sh
+++ zapakiraj_27303.sh
@@ -3,14 +3,19 @@
 # Exit immediately if a command exits with a non-zero status and for any failures in pipelines.
 set -eo pipefail
 
-# Load Docker environment variables, if any
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-fi
+# Assign command line arguments to variables
+DOCKER_USERNAME="$1"
+GITHUB_SHA="$2"
 
 # Check if DOCKER_USERNAME is set
 if [ -z "$DOCKER_USERNAME" ]; then
     echo "DOCKER_USERNAME is not set. Exiting..."
+    exit 1
+fi
+
+# Check if GITHUB_SHA is set
+if [ -z "$GITHUB_SHA" ]; then
+    echo "GITHUB_SHA is not set. Exiting..."
     exit 1
 fi
 
